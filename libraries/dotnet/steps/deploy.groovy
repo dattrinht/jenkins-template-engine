750b1d49b6fd9444de9132ce7cec04ad4e723f1b
dattt <dattt@creativeforce.io>
2022-05-18 10:24:22 +0700

    add debug println

--- libraries/dotnet/steps/deploy.groovy
+++ libraries/dotnet/steps/deploy.groovy
@@ -1,4 +1,4 @@
-void call(app_env, Map config){
+void call(Map config, app_env){
     stage("Deploy ${config.program_name}") {
         app_env.deploy_server_instances.each { entry -> 
             def instance = entry.split(" : ")
