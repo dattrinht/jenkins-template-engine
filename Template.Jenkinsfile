449f63f48e306b2d587cf6a0bfaf773592ea64f9
dattt <dattt@creativeforce.io>
2022-05-18 11:29:55 +0700

    add debug println

--- Template.Jenkinsfile
+++ Template.Jenkinsfile
@@ -7,7 +7,7 @@ pipeline {
                 pull_code git_repositories: env.git_repositories
 
                 script {
-                    def buildCommandSupervisor = command_builder.buildCommandSupervisor(OBJECT_S3: "aaa", SERVICE_NAME: "bbbb")
+                    def buildCommandSupervisor = command_builder.buildCommandSupervisor("aaa", "bbbb")
                     println "${buildCommandSupervisor}"
                     // def buildCommandDeploy = command_builder.buildCommandDeploy OBJECT_S3: OBJECT_S3, SERVICE_NAME: SERVICE_NAME
                     // println "${buildCommandDeploy}"
--- libraries/utilities/steps/command_builder.groovy
+++ libraries/utilities/steps/command_builder.groovy
@@ -1,4 +1,4 @@
-def buildCommandSupervisor(String OBJECT_S3, String SERVICE_NAME) {
+String buildCommandSupervisor(String OBJECT_S3, String SERVICE_NAME) {
     def command = """
             mkdir -p /etc/supervisor/conf.d/
             aws s3 cp --only-show-errors ${OBJECT_S3} /etc/supervisor/conf.d/${SERVICE_NAME}.conf
@@ -6,36 +6,36 @@ def buildCommandSupervisor(String OBJECT_S3, String SERVICE_NAME) {
     return command
 }
 
-def buildCommandDeploy(Map config) {
+String buildCommandDeploy(String SERVICE_NAME, String OBJECT_S3, String OUTPUT_NAME) {
     def command = """
-            mkdir -p /home/creativeforce/${config.SERVICE_NAME}
+            mkdir -p /home/creativeforce/${SERVICE_NAME}
             cd /home/creativeforce
-            aws s3 cp --only-show-errors ${config.OBJECT_S3} ${config.OUTPUT_NAME}
+            aws s3 cp --only-show-errors ${OBJECT_S3} ${OUTPUT_NAME}
             
-            supervisorctl stop ${config.SERVICE_NAME}
-            rm -rf /home/creativeforce/${config.SERVICE_NAME}/*
-            tar -xf ${config.OUTPUT_NAME} -C /home/creativeforce/${config.SERVICE_NAME}
+            supervisorctl stop ${SERVICE_NAME}
+            rm -rf /home/creativeforce/${SERVICE_NAME}/*
+            tar -xf ${OUTPUT_NAME} -C /home/creativeforce/${SERVICE_NAME}
             
             chown -R creativeforce:creativeforce /var/lighthouse
-            chown -R creativeforce:creativeforce /home/creativeforce/${config.SERVICE_NAME}
+            chown -R creativeforce:creativeforce /home/creativeforce/${SERVICE_NAME}
             
             supervisorctl reread
-            supervisorctl update ${config.SERVICE_NAME}
-            supervisorctl start ${config.SERVICE_NAME}
-            rm ${config.OUTPUT_NAME}
+            supervisorctl update ${SERVICE_NAME}
+            supervisorctl start ${SERVICE_NAME}
+            rm ${OUTPUT_NAME}
         """
     return command
 }
 
-def buildSupervisorConfigFile(Map config) {
+String buildSupervisorConfigFile(String SERVICE_NAME, String COMMAND) {
     def supervisor_config_value = """
-            [program:${config.SERVICE_NAME}]
-            command=${config.COMMAND}
-            directory=/home/creativeforce/${config.SERVICE_NAME}/
+            [program:${SERVICE_NAME}]
+            command=${COMMAND}
+            directory=/home/creativeforce/${SERVICE_NAME}/
             autostart=true
             autorestart=true
-            stderr_logfile=/var/log/supervisor/${config.SERVICE_NAME}.err.log
-            stdout_logfile=/var/log/supervisor/${config.SERVICE_NAME}.out.log
+            stderr_logfile=/var/log/supervisor/${SERVICE_NAME}.err.log
+            stdout_logfile=/var/log/supervisor/${SERVICE_NAME}.out.log
             environment=ASPNETCORE_ENVIRONMENT=${env.ASPNETCORE_ENVIRONMENT}
             user=creativeforce
             group=creativeforce
