5bd03b3eb9329f323ea824bedef4594f8fecc5eb
dattt <dattt@creativeforce.io>
2022-05-19 09:10:58 +0700

    add debug println

--- libraries/dotnet/src/app_static.groovy
+++ libraries/dotnet/src/app_static.groovy
@@ -1,9 +1,9 @@
-class app_static {
-    String base_file_by_time() {
+static class app_static {
+    static String base_file_by_time() {
         return """${new Date().format('yyMMddHHmmss', TimeZone.getTimeZone('UTC'))}"""
     }
 
-    String s3_dir_location() {
+    static String s3_dir_location() {
         return """s3://cf-sandbox-jenkins-temp/$JOB_NAME/${base_file_by_time()}/"""
     }
 }
