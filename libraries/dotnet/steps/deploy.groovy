261d3c035df6cb2fab5ff6e2b6feb73c938d3123
dattt <dattt@creativeforce.io>
2022-05-19 10:04:49 +0700

    add debug println

--- libraries/dotnet/steps/deploy.groovy
+++ libraries/dotnet/steps/deploy.groovy
@@ -1,5 +1,5 @@
-void call(Map config, app_env){
-    stage("Deploy ${config.program_name}") {
+void call(app_env){
+    stage("Deploy ${config.service_name}") {
         app_env.deploy_server_instances.each { entry -> 
             def instance = entry.split(" : ")
             echo """deploy to server name: ${instance[0]} with ip: ${instance[1]}"""
